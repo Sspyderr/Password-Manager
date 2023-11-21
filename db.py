63a084bea7b1f4ccfaf823a6556079ebde228bb2
Pramod Kumar <110077604+Sspyderr@users.noreply.github.com>
2023-11-21 23:10:16 +0530

    Add files via upload
    “Password Manager” is a small-scale project used for storing your login 
    information for all the websites you use and generating random passwords 
    for users. The user will have the access to add, update and remove his details 
    and he can fetch his data whenever needed.
    
    
    It’s an GUI based application simple and easy to use. It has a lot’s of
    feature which makes user experience better. In this application Passwords 
    can be stored for various applications and details required for maintaining
    the Passwords are application or website name, username, password and
    email id. 
    
    You would not get any problem while using it because it’s simple and
    user friendly UI will make your work easy, faster and gives you a better 
    result.

---
+++ db.py
@@ -0,0 +1,23 @@
+import mysql.connector
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='root')
+cur = db.cursor()
+cur.execute('CREATE DATABASE Password_manager')
+
+# ----------Table Creation----------
+import mysql.connector
+db = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='root',
+    database='Password_manager')
+cur = db.cursor()
+cur.execute("CREATE TABLE Accounts(userID int PRIMARY KEY AUTO_INCREMENT,Username VARCHAR(50),Password VARCHAR(30))")
+cur.execute("CREATE TABLE userdata (Website VARCHAR(50),"
+            "Username VARCHAR(50), "
+            "Password VARCHAR(50), "
+            "Email_id VARCHAR(50))"
+            )
+db.commit()
